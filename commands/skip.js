3df124687208b169a44e0a35246c2f6bf0b9e72c
Ignacio Rosas <ignacio105garcia@gmail.com>
2024-03-30 15:17:03 -0600

    Update skip.js
    add new skip command feature

---
+++ commands/skip.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("skip the current song"),
+    execute: async ({client, interaction}) => {
+        const queue = client.player.getQueue(interaction.guild);
+        if (!queue) {
+            await interaction.reply("There is no song playing");
+            return;
+        }
+        const currentSong = queue.current;
+        queue.skip();
+
+        await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                .setDescription(`Skipped --${currentSong.title}--`)
+                .setThumbnail(currentSong.thumbnail)
+            ]
+        })
+    }
+}
